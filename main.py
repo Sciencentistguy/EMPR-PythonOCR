ca9acd0d462e43fca1a2c3f00a7421cd81a0c115
Jamie Quigley <jamie@jamiequigley.com>
2020-03-06 14:17:23 +0000

    Added scale function

--- main.py
+++ main.py
@@ -34,6 +34,14 @@ def crop_unneeded(image: Image) -> Image:
     return Image.fromarray(array_cropped)
 
 
+def scale_keeping_aspect(array: np.array, height: int) -> np.array:
+    scale = float(len(array)) / height
+    width = int(round(len(array[0]) * height / len(array)))
+    scaled_array = np.zeros((height, width))
+    for x in range(width):
+        for y in range(height):
+            scaled_array[y, x] = array[int(floor(scale * y)), int(floor(scale*x))]
+    return scaled_array
 image = Image.open(sys.argv[1]).convert("L")
 bw = grayscale_to_monochrome(image)
 cropped = crop_unneeded(bw)
